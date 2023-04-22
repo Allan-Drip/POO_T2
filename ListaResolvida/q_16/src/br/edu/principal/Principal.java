fc18f76ae7c8eaea9c2562b452d258f0508002a6
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 20:02:03 -0300

    Update Principal.java

--- ListaResolvida/q_16/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_16/src/br/edu/principal/Principal.java
@@ -1,32 +1,36 @@
 package br.edu.principal; 
- import java.util.Scanner; 
-  
-public class Principal { 
+import java.util.Scanner;
 
-public static void main(String[] args) { 
-Scanner sc = new Scanner(System.in);
-double pre=0,venda,newpre=0;
+public class Principal {
+    public static void main(String[] args) {
+    	
+        System.out.println("***********************************");
+        System.out.println("Reajuste dos pre√ßos do supermercado");
+        System.out.println("***********************************\n");
+    	
+        Scanner sc = new Scanner(System.in);
+        double pre = 0, venda = 0, newpre = 0;
 
-System.out.print("Digite o preÁo atual do produto :  ");
-pre = sc.nextDouble();
+        System.out.println("Digite o pre√ßo atual do produto: ");
+        pre = sc.nextDouble();
 
-System.out.print("Digite a quantidade de venda mensal do produto:  ");
-venda = sc.nextDouble();
+        System.out.print("Digite a quantidade de venda mensal do produto: ");
+        venda = sc.nextDouble();
 
-if(venda < 500 || pre<30) {
-newpre = pre+(pre*0.10);   
-System.out.println("O valor do preÁo sofreu aumento de 10%, e ficou de: "+newpre);
-  }
-    
-if((venda >= 500 && venda < 1200 || pre >=30 && pre < 80)) {
-newpre = pre+(pre*0.15);
-System.out.println("O valor do preÁo sofreu aumento de 15%, e ficou de: "+newpre);
-}
-    
-if(venda > 1200 || pre>=80){
-newpre = pre-(pre*0.15);
-System.out.println("O valor do preÁo sofreu uma diminuiÁ„o de 20%, e ficou de: "+newpre);
-   
- }
-}
+        if (venda < 500 && pre < 30) {
+            newpre = pre * 1.1;
+            System.out.println("O valor do pre√ßo sofreu aumento de 10%, e ficou de: " + newpre);
+        }
+
+        if (venda >= 500 && venda < 1200 && pre >= 30 && pre < 80) {
+            newpre = pre * 1.15;
+            System.out.println("O valor do pre√ßo sofreu aumento de 15%, e ficou de: " + newpre);
+        }
+
+        if (venda >= 1200 && pre >= 80) {
+            newpre = pre * 0.8;
+            System.out.println("O valor do pre√ßo sofreu uma redu√ß√£o de 20%, e ficou de: " + newpre);
+        }
+
+    }
 }
