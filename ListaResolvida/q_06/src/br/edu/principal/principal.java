e3ac56f339d1c03acef2c6f5cd8536b45ac248c8
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 19:43:50 -0300

    Update principal.java

--- ListaResolvida/q_06/src/br/edu/principal/principal.java
+++ ListaResolvida/q_06/src/br/edu/principal/principal.java
@@ -12,16 +12,16 @@ public class Principal {
 
     	 
     	 Scanner sc = new Scanner(System.in);
-    	 double num1=0;
+    	 int num1=0;
 
     	 System.out.print("Digite o número: ");
-    	 num1 = sc.nextDouble();
+    	 num1 = sc.nextInt();
 
     	 if(num1 % 2 == 0){
-    		 System.out.println("É par !");
+    		 System.out.println("É par! \n");
     	 }
 
     	 else{
-    		 System.out.println("É impar!");}
+    		 System.out.println("É impar! \n");}
 }
 }
